1b12a0d61e4fc9068b9184cba17783826ebb1bb0
Hieu Nguyen Xuan <hieu.nguyenxuan@decision-science.com>
2024-11-01 16:00:07 +0700

    add: jenkinsfile

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credential', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t xuanhieu073/demo-app:jma-2.0 .'
+        sh 'docker build -t xuanhieu073/ems:1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push xuanhieu073/demo-app:jma-2.0"
+        sh "docker push xuanhieu073/ems:1.0"
     }
 }
 
