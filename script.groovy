db597561b89f35b0336eee4ba294ba3bcfc2b3ca
Nguyen Xuan Hieu <xuanhieu073@gmail.com>
2024-12-04 03:28:48 +0700

    fix jenkins build

--- script.groovy
+++ script.groovy
@@ -17,8 +17,8 @@ def deployApp() {
     // def dockerCmd = "docker run -p 8080:8080 -d xuanhieu073/demo-app:jma-2.0"
     def dockerComposeCmd = "docker-compose -f docker-compose.yaml up --detach"
     sshagent(['my-server-key']) {
-        sh "scp docker-compose.yaml ec2-user@ec2-52-221-240-115.ap-southeast-1.compute.amazonaws.com:/home/ec2-user"
         sh "ssh -o StrictHostKeyChecking=no ec2-user@ec2-52-221-240-115.ap-southeast-1.compute.amazonaws.com ${dockerComposeCmd}"
+        sh "scp docker-compose.yaml ec2-user@ec2-52-221-240-115.ap-southeast-1.compute.amazonaws.com:/home/ec2-user"
     }
 }
 
