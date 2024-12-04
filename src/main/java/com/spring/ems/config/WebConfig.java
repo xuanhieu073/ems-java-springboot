166e4fc40f8d5903801a5845ac27c0895aff76ca
Nguyen Xuan Hieu <xuanhieu073@gmail.com>
2024-12-04 18:38:31 +0700

    fix cors

--- src/main/java/com/spring/ems/config/WebConfig.java
+++ src/main/java/com/spring/ems/config/WebConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://ems-fe-zeta.vercel.app", "http://localhost:4200");
+        registry.addMapping("/**").allowedOrigins("https://ems-fe-zeta.vercel.app");
     }
 }
